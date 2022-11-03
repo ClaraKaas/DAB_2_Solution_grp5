4bcb92ba99154cf0035449ceb8796e74dd1de40f
Clara <claramouritsen@outlook.com>
2022-11-03 11:05:46 +0100

    Added MaintenanceLog

---
+++ DAB_2_Solution_grp5/Models/MaintenanceLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_2_Solution_grp5.Models
+{
+    internal class MaintenanceLog
+    {
+        public int MaintenanceId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
