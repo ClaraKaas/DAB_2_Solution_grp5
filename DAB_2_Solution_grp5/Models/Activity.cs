c9de9147cca74134472fabcf78db2ef0fd8ffd55
Clara <claramouritsen@outlook.com>
2022-11-03 11:02:04 +0100

    Added attributes to activity

---
+++ DAB_2_Solution_grp5/Models/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_2_Solution_grp5.Models
+{
+    internal class Activity
+    {
+        public int BookingId { get; set; }
+        public DateTime Time { get; set; }
+        public string Note { get; set; }
+        public int Participants { get; set; }
+    }
+}
