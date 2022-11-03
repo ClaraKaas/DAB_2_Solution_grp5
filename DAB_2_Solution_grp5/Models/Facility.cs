d51df0c9f7232bf0940ff11d2407e858b1f1e1fd
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-11-03 11:04:14 +0100

    Facility

--- DAB_2_Solution_grp5/Models/Facility.cs
+++ DAB_2_Solution_grp5/Models/Facility.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAB_2_Solution_grp5.Models
 {
-    internal class Facility
+    public class Facility
     {
+        [Key]
+        public int FacilityID { get; set; }
+        public string Name { get; set; }
+        public string? Address { get; set; }
+
+        public string? Type { get; set; }
+        public string? Description { get; set; }
+
+        public string? Bookable { get; set; }
+
+        public string? Items { get; set; }
+
+
     }
 }
