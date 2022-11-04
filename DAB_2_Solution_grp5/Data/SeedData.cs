f4fc78bb45b0ee3e9c7643269d20c23c4051cd54
Clara <claramouritsen@outlook.com>
2022-11-04 13:24:03 +0100

    changes to seeding

--- DAB_2_Solution_grp5/Data/SeedData.cs
+++ DAB_2_Solution_grp5/Data/SeedData.cs
@@ -9,6 +9,7 @@ using System.Diagnostics.Metrics;
 using System.Net.Sockets;
 using System.Net;
 
+
 namespace DAB_2_Solution_grp5.Data
 {
     public class SeedData
@@ -27,7 +28,6 @@ namespace DAB_2_Solution_grp5.Data
 
             Facility AarhusStrand = new Facility()
             {
-                FacilityId = 1234567,
                 Name = "AarhusStrand",
                 Address = "Strand 9",
                 Type = "Privat",
@@ -39,7 +39,7 @@ namespace DAB_2_Solution_grp5.Data
 
             Facility Navitas = new Facility()
             {
-                FacilityId = 35899387,
+
                 Name = "Navitas",
                 Address = "Møllevej 15",
                 Type = "forretning",
@@ -62,7 +62,7 @@ namespace DAB_2_Solution_grp5.Data
             };
             Activity Activity2 = new Activity()
             {
-                ActivityId = 2,
+      
                 Time = DateTime.Now.Add(TimeSpan.FromHours(15)),
                 Note = "jnjcxdzrtfyguhijokpszxrtfgyhuijokl",
                 Participants = 10,
@@ -74,7 +74,7 @@ namespace DAB_2_Solution_grp5.Data
 
             User User1 = new User()
             {
-                UserId = 2022,
+    
                 Name = "Clara",
                 Email = "clara@gmail.com",
                 CVR = "109876543",
@@ -85,7 +85,7 @@ namespace DAB_2_Solution_grp5.Data
 
             User User2 = new User()
             {
-                UserId = 2030,
+             
                 Name = "Heja",
                 Email = "Heja@gmail.com",
                 CVR = "098765432",
@@ -97,39 +97,12 @@ namespace DAB_2_Solution_grp5.Data
             db.Add(User2);
 
 
-            Booking booking1 = new Booking()
-            {
-                BookingId = 12,
-                ActivityId = 1,
-                UserId = 2022,
-                FacilityId = 1234567
-
-            };
-
-            Booking booking2 = new Booking()
-            {
-
-                BookingId = 14,
-                ActivityId = 2,
-                UserId = 2030,
-                FacilityId = 35899387
-
-
-            };
-
-            db.Add(booking1);
-            db.Add(booking2);
-
             Personnel Personnel1 = new Personnel()
-            {
-
-                PersId = 14
-            };
+            {    };
 
             Personnel Personnel2 = new Personnel()
             {
 
-                PersId = 15
             };
 
             db.Add(Personnel1);
@@ -138,7 +111,6 @@ namespace DAB_2_Solution_grp5.Data
             MaintenanceLog MaintenanceLog1 = new MaintenanceLog()
             {
 
-                MaintenanceId = 1955,
                 Description = "Ved ikke",
                 Date = DateTime.Now.Add(TimeSpan.FromHours(15)),
 
@@ -147,7 +119,7 @@ namespace DAB_2_Solution_grp5.Data
             MaintenanceLog MaintenanceLog2 = new MaintenanceLog()
             {
 
-                MaintenanceId = 1966,
+     
                 Description = "gegikvep",
                 Date = DateTime.Now.Add(TimeSpan.FromHours(15)),
 
@@ -155,29 +127,6 @@ namespace DAB_2_Solution_grp5.Data
             db.Add(MaintenanceLog1);
             db.Add(MaintenanceLog2);
 
-            Does_Maintenance Does_Maintenanc1 = new Does_Maintenance()
-            {
-
-                Does_MaintenanceId = 19,
-                FacilityId = 1234567,
-                MaintenanceId = 1955,
-                PersId = 14
-               
-
-            };
-
-            Does_Maintenance Does_Maintenanc2 = new Does_Maintenance()
-            {
-
-                Does_MaintenanceId = 15,
-                FacilityId = 35899387,
-                MaintenanceId = 1966,
-                PersId = 15
-
-
-            };
-
-            db.Add(Does_Maintenanc1);
 
             Console.WriteLine("Seeding completed successfully!");
 
